0b9fe6817aac340864ad9ec3c4675f3b8f567354
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 12:17:11 +0300

    FullNameReadFromDataException

---
+++ src/Lesson3Dz/base/exceptions/FullNameReadFromDataException.java
@@ -0,0 +1,7 @@
+package Lesson3Dz.base.exceptions;
+
+public class FullNameReadFromDataException extends IndexOutOfBoundsException{
+    public FullNameReadFromDataException(String s) {
+        super("Parse full name exception: " + s);
+    }
+}
