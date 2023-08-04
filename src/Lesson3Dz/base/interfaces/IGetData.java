620c2ecc7f8ca6b61c6c5504011364582f4396b8
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 13:10:10 +0300

    IGetData

---
+++ src/Lesson3Dz/base/interfaces/IGetData.java
@@ -0,0 +1,9 @@
+package Lesson3Dz.base.interfaces;
+
+import Lesson3Dz.base.exceptions.GetDataException;
+
+import java.io.IOException;
+
+public interface IGetData {
+    public String getData() throws IOException;
+}
