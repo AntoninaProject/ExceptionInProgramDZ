05cba497ea7a69da575a643b748d76209b3e28a7
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 10:38:53 +0300

    AFindTheSameFileName

---
+++ src/Lesson3Dz/base/abstractClasses/AFindTheSameFileName.java
@@ -0,0 +1,10 @@
+package Lesson3Dz.base.abstractClasses;
+
+import Lesson3Dz.base.interfaces.IFindTheSameFileName;
+
+import java.io.File;
+
+public abstract class AFindTheSameFileName implements IFindTheSameFileName {
+    protected File folder;
+
+}
