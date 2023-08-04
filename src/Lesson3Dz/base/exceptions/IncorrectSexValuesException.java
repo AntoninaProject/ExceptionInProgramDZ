c252cf9ea40692f03156a49046e17fe729e8102c
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 12:21:52 +0300

    IncorrectSexValuesException

---
+++ src/Lesson3Dz/base/exceptions/IncorrectSexValuesException.java
@@ -0,0 +1,7 @@
+package Lesson3Dz.base.exceptions;
+
+public class IncorrectSexValuesException extends RuntimeException{
+    public IncorrectSexValuesException() {
+        super("Incorrect sex value!!!");
+    }
+}
