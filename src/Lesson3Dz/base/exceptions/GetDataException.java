cb30efa3dbf0fcab506bb46bd391e68c58d07769
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 12:20:29 +0300

    GetDataException

---
+++ src/Lesson3Dz/base/exceptions/GetDataException.java
@@ -0,0 +1,9 @@
+package Lesson3Dz.base.exceptions;
+
+import java.io.IOException;
+
+public class GetDataException extends IOException {
+    public GetDataException(IOException message) {
+        super("Exception while reading from console: " + message.getMessage());
+    }
+}
