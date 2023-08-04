0449640ac77f2a0f1dd2342b624ca5ba714c1e0c
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 12:12:26 +0300

    FileCreateException

---
+++ src/Lesson3Dz/base/exceptions/FileCreateException.java
@@ -0,0 +1,9 @@
+package Lesson3Dz.base.exceptions;
+
+import java.io.IOException;
+
+public class FileCreateException extends IOException {
+    public FileCreateException(String message) {
+        super("File isn't created: " + message);
+    }
+}
