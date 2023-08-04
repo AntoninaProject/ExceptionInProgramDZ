bc2a49cf6e4e66f6abfe2b83277e2ce465bb74d4
Antonina Petrova <ant2466@yandex.ru>
2023-08-04 10:30:56 +0300

    ADataParseProcessor

---
+++ src/Lesson3Dz/base/abstractClasses/ADataParseProcessor.java
@@ -0,0 +1,7 @@
+package Lesson3Dz.base.abstractClasses;
+
+import Lesson3Dz.base.interfaces.IGetInfo;
+import Lesson3Dz.base.interfaces.IParseData;
+
+public abstract class ADataParseProcessor implements IGetInfo, IParseData {
+}
